a9c6565c6c586c80da986eaa7d2facfc024b1947
Eduardo Moreno <edmolo_0@hotmail.com>
2018-05-29 00:10:43 -0600

    repo borrado

--- appComidita/app/src/main/java/com/example/supdude/appcomidita/OnEditTextChanged.java
+++
@@ -1,5 +0,0 @@
-package com.example.supdude.appcomidita;
-
-public interface OnEditTextChanged {
-    void onTextChanged(int position, String charSeq);
-}
--- appComidita/app/src/main/java/com/example/supdude/appcomidita/RecyclerViewOnItemClickListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.supdude.appcomidita;
-
-import android.view.View;
-
-
-public interface RecyclerViewOnItemClickListener {
-
-    void onClick(View v, int position);
-}
